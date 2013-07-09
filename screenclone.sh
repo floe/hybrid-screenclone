af88a33fef2f0f185f806e57e9f0a6e2f41b9354
Florian Echtler <floe@butterbrot.org>
2013-07-09 16:23:24 +0200

    minor tweaks to wrapper script

--- screenclone.sh
+++ screenclone.sh
@@ -8,10 +8,10 @@ MODE=${X}x${Y}
 xrandr --newmode $MODE $(echo ${X}\*${Y}\*60 | bc) $X $X $X $X $Y $Y $Y $Y
 xrandr --addmode VIRTUAL $MODE
 xrandr --output  VIRTUAL --mode $MODE --$DIR LVDS1
-sleep 2
 
 # put local xorg.conf.nvidia into /etc/bumblebee/
-optirun sleep 1e10d &
-sleep 2
+optirun sleep 1e10d & disown
+sleep 3
 
-screenclone -s :0 -d :8 -x 1
+cd $(dirname $0)
+./screenclone -s :0 -d :8 -x 1 & disown
